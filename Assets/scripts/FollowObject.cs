584bbe3535a21410fc21ddbd435d495a6ab7153a
Malboro <toxyin@protonmail.com>
2025-05-11 00:08:02 +0200

    features/lighting - simple lighting for ennemies

--- Assets/scripts/FollowObject.cs
+++ Assets/scripts/FollowObject.cs
@@ -6,6 +6,11 @@ public class FollowObject : MonoBehaviour
 {
     public GameObject gameObject;
     public GameObject target;
+    public bool followVertical = false;
+    public bool followLook = false;
+    public float yOffset = 0f;
+    public float xOffset = 0f;
+    public float zOffset = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +21,10 @@ public class FollowObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        target.transform.position = new Vector3(gameObject.transform.position.x, target.transform.position.y, gameObject.transform.position.z);    
+        target.transform.position = new Vector3(gameObject.transform.position.x + xOffset, (followVertical ? gameObject.transform.position.y : target.transform.position.y) + yOffset, gameObject.transform.position.z + zOffset);    
+        if (followLook)
+        {
+            target.transform.rotation = gameObject.transform.rotation;
+        }
     }
 }
