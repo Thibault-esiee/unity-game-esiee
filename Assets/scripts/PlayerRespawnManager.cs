b6c12ceef8e495b3c9e1b773ef1670edd703700c
Thibault-esiee <thibault.livran@edu.esiee.fr>
2025-05-18 23:24:21 +0200

    update final dialogue

--- Assets/scripts/PlayerRespawnManager.cs
+++ Assets/scripts/PlayerRespawnManager.cs
@@ -5,98 +5,95 @@ using UnityEngine.SceneManagement;
 
 public class PlayerRespawnManager : MonoBehaviour
 {
-    [Header("Respawn Settings")]
-    [SerializeField] private Transform respawnPoint;
-    [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private GameObject deathBody;
     
-    [Header("Respawn UI")]
-    [SerializeField] private GameObject respawnUI;
+    
+    // [Header("Respawn UI")]
+    // [SerializeField] private GameObject respawnUI;
 
-    private PlayerController currentPlayerController;
-    private static PlayerRespawnManager instance;
+    // private PlayerController currentPlayerController;
+    // private static PlayerRespawnManager instance;
 
-    private void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+    // private void Awake()
+    // {
+    //     if (instance == null)
+    //     {
+    //         instance = this;
+    //         DontDestroyOnLoad(gameObject);
+    //     }
+    //     else
+    //     {
+    //         Destroy(gameObject);
+    //     }
+    // }
 
-    private void Start()
-    {
-        if (respawnUI != null)
-            respawnUI.SetActive(false);
-    }
-    public void OnRespawn(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            TryRespawn();
-        }
-    }
+    // private void Start()
+    // {
+    //     if (respawnUI != null)
+    //         respawnUI.SetActive(false);
+    // }
+    // public void OnRespawn(InputAction.CallbackContext context)
+    // {
+    //     if (context.performed)
+    //     {
+    //         TryRespawn();
+    //     }
+    // }
 
-    public void TryRespawn()
-    {
-        GameObject currentPlayer = GameObject.FindGameObjectWithTag("Player");
+    // public void TryRespawn()
+    // {
+    //     GameObject currentPlayer = GameObject.FindGameObjectWithTag("Player");
         
-        if (currentPlayer != null)
-        {
-            PlayerController playerController = currentPlayer.GetComponent<PlayerController>();
+    //     if (currentPlayer != null)
+    //     {
+    //         PlayerController playerController = currentPlayer.GetComponent<PlayerController>();
             
 
-            if (playerController != null && !playerController.IsPlayerAlive())
-            {
-                RespawnPlayer();
-            }
-        }
-    }
+    //         if (playerController != null && !playerController.IsPlayerAlive())
+    //         {
+    //             RespawnPlayer();
+    //         }
+    //     }
+    // }
 
-    public void RespawnPlayer()
-    {
-        GameObject currentPlayer = GameObject.FindGameObjectWithTag("Player");
+    // public void RespawnPlayer()
+    // {
+    //     GameObject currentPlayer = GameObject.FindGameObjectWithTag("Player");
         
-        if (currentPlayer != null)
-        {
-            PlayerController playerController = currentPlayer.GetComponent<PlayerController>();
+    //     if (currentPlayer != null)
+    //     {
+    //         PlayerController playerController = currentPlayer.GetComponent<PlayerController>();
             
 
-            if (playerController.DeathBody != null)
-            {
-                Destroy(playerController.DeathBody);
-            }
+    //         if (playerController.DeathBody != null)
+    //         {
+    //             Destroy(playerController.DeathBody);
+    //         }
             
 
-            if (respawnPoint != null)
-            {
-                currentPlayer.transform.position = respawnPoint.position;
-                currentPlayer.transform.rotation = respawnPoint.rotation;
-            }
+    //         if (respawnPoint != null)
+    //         {
+    //             currentPlayer.transform.position = respawnPoint.position;
+    //             currentPlayer.transform.rotation = respawnPoint.rotation;
+    //         }
             
 
-            playerController.Respawn();
+    //         playerController.Respawn();
             
 
-            if (respawnUI != null)
-                respawnUI.SetActive(false);
+    //         if (respawnUI != null)
+    //             respawnUI.SetActive(false);
             
 
-            NotifyEnemiesOfRespawn();
-        }
-    }
+    //         NotifyEnemiesOfRespawn();
+    //     }
+    // }
 
-    private void NotifyEnemiesOfRespawn()
-    {
-        NPC_Enemy[] enemies = FindObjectsOfType<NPC_Enemy>();
-        foreach (NPC_Enemy enemy in enemies)
-        {
-            enemy.OnPlayerRespawn();
-        }
-    }
+    // private void NotifyEnemiesOfRespawn()
+    // {
+    //     NPC_Enemy[] enemies = FindObjectsOfType<NPC_Enemy>();
+    //     foreach (NPC_Enemy enemy in enemies)
+    //     {
+    //         enemy.OnPlayerRespawn();
+    //     }
+    // }
 }
